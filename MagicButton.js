ffe069ffa2c265e7fc0c24eda939b1dd44f87fd3
David H <davidhorph@hotmail.com>
2018-07-04 16:56:17 -0500

    ...

--- MagicButton.js
+++ MagicButton.js
@@ -1,14 +1,29 @@
 
 let count = 0;
+let timercount = 0;
 
-$(document).ready(function() {
-    $("h1").click(
-        function() {
-            count == 15 ? count=0 : count+=1;
-            $("#mycount").text(count);
-            $(this).toggleClass("JToggle");
-        }
-    );
-})
+let click = document.querySelector(".click");
+let h3 = document.querySelector("h3");
+
+let timer = setInterval(function() {
+    timercount +=1;
+},3000);
+
+// $(document).ready(function() {
+//     $("h1").click(
+//         function() {
+//             count == 15 ? count=0 : count+=1;
+//             $("#mycount").text(count);
+//             $(this).toggleClass("JToggle");
+//         }
+//     );
+// }) 
+
+
+timercount > 5 ? click.classList.add("hideelement") : timercount;
+
+timer;
+h3.textContent = timercount;
+hide();
 
 
