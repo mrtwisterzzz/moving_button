a826f708191fc921fbfc9ea741c3520747e71e0b
David H <davidhorph@hotmail.com>
2018-07-04 13:44:38 -0500

    Added ToggleClass

--- MagicButton.js
+++ MagicButton.js
@@ -1,13 +1,14 @@
 
 let count = 0;
 
-$(document).ready(function() { 
-    let count = 0;
-    $("h1").click(function() {
-        $(this).click(function() {
-            count++;
-        })
-    })
-}) 
+$(document).ready(function() {
+    $("h1").click(
+        function() {
+            count == 15 ? count=0 : count+=1;
+            $("#mycount").text(count);
+            $(this).toggleClass("JToggle");
+        }
+    );
+})
 
-document.getElementById("mycount").innerHTML = count;
+
