5d39a649a6e5e4c63a9eb86d5026e4fef6c5d196
David H <davidhorph@hotmail.com>
2018-07-04 22:37:26 -0500

    Added span
    Will try to show counter backwards.

--- MagicButton.js
+++ MagicButton.js
@@ -2,14 +2,14 @@
 let count = 0;
 
 let click = document.querySelector("#click");
-let h3 = document.querySelector("h3");
+let span = document.querySelector("span");
 
 
 let timer = setInterval(hide,1500);
 
 function hide() {
     count +=1;
-    h3.textContent = count;
+    h3.textContent = 5-count;
 
     $("#click").click(function() {
         clearInterval(1);
