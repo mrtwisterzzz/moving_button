7c881b2b32751c21e610ff426ec01ba99321ff34
Your Name <you@example.com>
2021-10-19 17:00:31 +0300

    start

--- MagicButton.js
+++ MagicButton.js
@@ -1,31 +1,12 @@
-
-let count = -1;
-
-let click = document.querySelector("#click");
-let span = document.querySelector("span");
-
-
-let timer = setInterval(hide,1500);
-
-function hide() {
-    count +=1;
-    span.textContent = 5-count;
-
-    $("#click").click(function() {
-        clearInterval(1);
-        $(this).css({"color" : "red"});        
-    }) 
-
-    if(count==5) 
-    {
-        click.classList.add("hideelement");
-        clearInterval(1);
-    }
-}
-
-timer;
-
-
-
-
-
+var coorA;
+var coorB;
+var btn = document.getElementsByTagName("button");
+var lastMouseX;
+var lastMouseY;
+console.log(btn);
+window.onload = function() {
+        for( i = 0; i<300; i++){
+            btn.style.top = btn.offsetTop + i;
+            btn.style.left = btn.offsetLeft + i;
+        }
+  };
